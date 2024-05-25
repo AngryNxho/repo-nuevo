d1f8f8416a7a24dafcf58e853bc37d12e3301438
= <=>
2024-05-25 19:18:17 -0400

    DONE

--- ConsultasBDD/Semana2/Desafios/e1_ignacio_manriquez_s2.sql
+++ ConsultasBDD/Semana2/Desafios/e1_ignacio_manriquez_s2.sql
@@ -1,3 +1,4 @@
+--CASO 1
 ALTER SESSION SET NLS_DATE_FORMAT='DD-mm-yy';
 SELECT EXTRACT(YEAR FROM SYSDATE)"AÑO TRIBUTARIO",
     (TO_CHAR(numrun_emp,'99G999G999') 
@@ -14,7 +15,30 @@ SELECT EXTRACT(YEAR FROM SYSDATE)"AÑO TRIBUTARIO",
     CASE 
     WHEN (EXTRACT(YEAR FROM SYSDATE)) - EXTRACT(YEAR FROM fecha_contrato) > 1
     THEN TRUNC(sueldo_base * 12 * (EXTRACT(YEAR FROM SYSDATE) -1 - EXTRACT(YEAR FROM fecha_contrato)) / 100)
-    END AS "BONOS POR AÑO ACTUAL"
+    ELSE 0
+    END AS "BONOS POR AÑO ACTUAL",
+    ROUND((sueldo_base * 0.12) * 12) "MOVILIZACION ANUAL"
+
 FROM empleado
 ORDER BY numrun_emp ASC;
-
+--CASO 2
+SELECT
+    ROUND((sueldo_base * 0.12) * 12) "MOVILIZACION ANUAL",
+    ROUND((sueldo_base * .2) * 12) "COLACION ANUAL",
+    (sueldo_base * 12 + 
+    CASE 
+        WHEN (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM fecha_contrato)) > 1
+        THEN TRUNC(sueldo_base * 12 * (EXTRACT(YEAR FROM SYSDATE) - 1 - EXTRACT(YEAR FROM fecha_contrato)) / 100)
+        ELSE 0
+    END + 
+    ROUND((sueldo_base * 0.12) * 12) + 
+    ROUND((sueldo_base * 0.2) * 12)) AS "SUELDO BRUTO",
+    (sueldo_base * 12 + CASE 
+        WHEN (EXTRACT(YEAR FROM SYSDATE) - EXTRACT(YEAR FROM fecha_contrato)) > 1
+        THEN TRUNC(sueldo_base * 12 * (EXTRACT(YEAR FROM SYSDATE) - 1 - EXTRACT(YEAR FROM fecha_contrato)) / 100)
+        ELSE 0
+    END) "RENTA IMPONIBLE ANUAL"
+FROM empleado
+ORDER BY numrun_emp ASC;
+--•	Sueldo bruto: sueldo base + bono por años trabajados + movilización + colación.
+--•	Renta imponible afecta a impuesto único: sueldo base + bono por años trabajados.

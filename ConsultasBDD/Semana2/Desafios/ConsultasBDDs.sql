7760307b2e36351ab01dd85d719dac491efa401b
= <=>
2024-05-25 12:30:32 -0400

    r

--- ConsultasBDD/Semana2/Desafios/ConsultasBDDs.sql
+++ ConsultasBDD/Semana2/Desafios/ConsultasBDDs.sql
@@ -60,3 +60,15 @@ TRUNC(TO_CHAR(SYSDATE,'MM')) "CLAVE"
 FROM empleado
 ORDER BY appaterno_emp;
 
+-- DESAFIO 5
+SELECT TO_CHAR(SYSDATE, 'MM/YYYY') MES_ANNO_PROCESO,
+nro_patente,
+fecha_ini_arriendo,
+dias_solicitados,
+fecha_devolucion,
+fecha_devolucion - fecha_ini_arriendo DIAS_ATRASO,
+(fecha_devolucion - fecha_ini_arriendo) * &valor_diario VALOR_MULTA
+FROM arriendo_camion
+WHERE fecha_devolucion >= ADD_MONTHS(TRUNC(SYSDATE,'MM'),-1)
+AND fecha_devolucion <= LAST_DAY(ADD_MONTHS(TRUNC(SYSDATE,'MM'),-1))
+ORDER BY fecha_ini_arriendo, nro_patente;

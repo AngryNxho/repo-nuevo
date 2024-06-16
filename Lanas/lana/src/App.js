382fd17c43150b1771170296a14ad239af1db2ee
IgnacioMS0 <127463774+IgnacioMS0@users.noreply.github.com>
2024-06-16 03:00:19 -0400

    a

--- Lanas/lana/src/App.js
+++ Lanas/lana/src/App.js
@@ -6,60 +6,30 @@ function Table() {
   const [value, setValue] = useState(0);
 
   function handleClick() {
-    alert("Clicked")
+    setValue(value + 1);
   }
 
   return (
-    <button onClick={handleClick}>X</button>
+          <button onClick={handleClick}>{value}</button>
   )
 }
+
+
 
 export default function App() {
   return (
-    <div>
-      <table>
-        <tr>
-          <td>
-            <Table value={value}/>
-            <Table value={value}/>
-            <Table value={value}/>
-          </td>
-        </tr>
+    <div className="tableContainer">
+        <div className="tableContent">
+          <div className="box">
+              <Table className="button-left"/>
+              <Table className="button-right"/>
+              <Table className="button-left"/>
+              <Table className="button-right"/>
 
-        <tr>
-          <td>
-            <Table value={value}/>
-            <Table value={value}/>
-            <Table value={value}/>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <Table value={value}/>
-            <Table value={value}/>
-            <Table value={value}/>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <Table value={value}/>
-            <Table value={value}/>
-            <Table value={value}/>
-          </td>
-        </tr>
-
-        <tr>
-          <td>
-            <Table value={value}/>
-            <Table value={value}/>
-            <Table value={value}/>
-          </td>
-        </tr>
-      </table>
-      
+          </div>
+        </div>
     </div>
   )
 }
+
 

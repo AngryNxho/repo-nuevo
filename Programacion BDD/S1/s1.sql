ac6f5856ef96fd427e6eddfcea310043340165e3
= <ig.manriquezs@duocuc.cl>
2024-08-12 17:09:31 -0400

    a

--- Programacion BDD/S1/s1.sql
+++ Programacion BDD/S1/s1.sql
@@ -58,10 +58,8 @@ DECLARE
     v_esp_id NUMBER := 1;                
     v_cantidad NUMBER;                   
 BEGIN 
-    -- Llamar a la funciÃ³n y almacenar el resultado en v_cantidad
     v_cantidad := periodo_especialidad(v_esp_id, v_periodo);
     
-    -- Imprimir el resultado
     DBMS_OUTPUT.PUT_LINE('Cantidad de atenciones para la especialidad ' || v_esp_id || ' en el periodo ' || v_periodo || ': ' || v_cantidad);
 END;
 /

b8ceac4b9d3c888b16307747039efece37cfc506
= <ig.manriquezs@duocuc.cl>
2024-08-15 01:18:05 -0400

    <

--- Programacion BDD/S1/e1_ignacio_manriquez_s1.sql
+++ Programacion BDD/S1/e1_ignacio_manriquez_s1.sql
@@ -1,38 +1,34 @@
-
-
--- -- funcion 1
--- CREATE OR REPLACE FUNCTION fn_periodo_atencion(fec_periodo IN VARCHAR2) 
--- RETURN NUMBER IS 
---     v_cantidad NUMBER;
+-- funcion 1
+CREATE OR REPLACE FUNCTION fn_periodo_atencion(fec_periodo IN VARCHAR2) 
+RETURN NUMBER IS 
+    v_cantidad NUMBER;
     
--- BEGIN
---     SELECT 
---         COUNT(*)
---     INTO
---         v_cantidad
---     FROM 
---         atencion
---     WHERE TO_CHAR(fecha_atencion, 'mm-yyyy') = fec_periodo;
---     RETURN v_cantidad;
--- END;
+BEGIN
+    SELECT 
+        COUNT(*)
+    INTO
+        v_cantidad
+    FROM 
+        atencion
+    WHERE TO_CHAR(fecha_atencion, 'mm-yyyy') = fec_periodo;
+    RETURN v_cantidad;
+END;
 
 
--- SET SERVEROUTPUT ON;
+SET SERVEROUTPUT ON;
 
--- DECLARE
---     v_periodo VARCHAR(7) := '10-2022';
---     v_cantidad NUMBER;
+DECLARE
+    v_periodo VARCHAR(7) := '10-2022';
+    v_cantidad NUMBER;
     
--- BEGIN 
---     v_cantidad := fn_periodo_atencion(v_periodo);
---     DBMS_OUTPUT.PUT_LINE('+++++++++++++++++++++++++++++++++++++++++');
---     DBMS_OUTPUT.PUT_LINE('------------Total atenciones: ' || v_cantidad);
--- END;
+BEGIN 
+    v_cantidad := fn_periodo_atencion(v_periodo);
+    DBMS_OUTPUT.PUT_LINE('+++++++++++++++++++++++++++++++++++++++++');
+    DBMS_OUTPUT.PUT_LINE('------------Total atenciones: ' || v_cantidad);
+END;
 
 
--- -- funcion 2
-
-
+-- funcion 2
 
 CREATE OR REPLACE FUNCTION periodo_especialidad(p_esp_id IN NUMBER, fec_periodo IN VARCHAR2) 
 RETURN NUMBER IS
@@ -62,7 +58,6 @@ BEGIN
     
     DBMS_OUTPUT.PUT_LINE('Cantidad de atenciones para la especialidad ' || v_esp_id || ' en el periodo ' || v_periodo || ': ' || v_cantidad);
 END;
-/
 
 
 

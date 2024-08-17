09debe60ff167701f880f266a4e457066080a36c
= <=>
2024-08-17 04:19:21 -0400

    bdd f2

--- Programacion BDD/S2/main.sql
+++ Programacion BDD/S2/main.sql
@@ -21,3 +21,28 @@ BEGIN
     v_dato := fn_cantidad_atenciones('11959315', '06-2024');
     DBMS_OUTPUT.PUT_LINE('Cantidad de atenciones: ' || v_dato);
 END;
+
+-- Funcion 2
+
+CREATE OR REPLACE FUNCTION fn_promedio_sueldos(p_id_cargo IN NUMBER)
+RETURN NUMBER IS
+
+v_sueldo_promedio NUMBER;
+
+BEGIN
+   SELECT ROUND(AVG(sueldo_base))
+    INTO v_sueldo_promedio
+    FROM medico
+    WHERE car_id = p_id_cargo;
+    
+    RETURN v_sueldo_promedio;
+END;
+
+SET SERVEROUTPUT ON;
+DECLARE
+v_dato NUMBER;
+
+BEGIN 
+    v_dato := fn_promedio_sueldos(100);
+    DBMS_OUTPUT.PUT_LINE('resultado: ' || v_dato);
+END;
